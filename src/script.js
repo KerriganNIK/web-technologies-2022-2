9cfa2ea9555fa6c5f81139860c6f49ea2830ebeb
nikit <nikita-ko85@mail.ru>
2022-12-29 14:48:47 +0500

    lesson9 finished lesson

---
+++ src/script.js
@@ -0,0 +1,128 @@
+if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', init)
+} else {
+    init()
+}
+
+function init() {
+    const data = {
+        name: 'Каталог товаров',
+        hasChildren: true,
+        items: [
+            {
+                name: 'Мойки',
+                hasChildren: true,
+                items: [
+                    {
+                        name: 'Ulgran1',
+                        hasChildren: true,
+                        items: [
+                            {
+                                name: 'SMT1',
+                                hasChildren: false,
+                                items: []
+                            },
+                            {
+                                name: 'SMT2',
+                                hasChildren: false,
+                                items: []
+                            }
+                        ]
+                    },
+                    {
+                        name: 'Ulgran2',
+                        hasChildren: true,
+                        items: [
+                            {
+                                name: 'SMT3',
+                                hasChildren: false,
+                                items: []
+                            },
+                            {
+                                name: 'SMT4',
+                                hasChildren: false,
+                                items: []
+                            }
+                        ]
+                    }
+                ]
+            }, {
+                name: 'Фильтры',
+                hasChildren: true,
+                items: [
+                    {
+                        name: 'Ulgran3',
+                        hasChildren: true,
+                        items: [
+                            {
+                                name: 'SMT5',
+                                hasChildren: false,
+                                items: []
+                            },
+                            {
+                                name: 'SMT6',
+                                hasChildren: false,
+                                items: []
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+    }
+
+
+    const items = new ListItems(document.getElementById('list-items'), data)
+
+    items.render()
+    items.init()
+
+    function ListItems(el, data) {
+        this.el = el;
+        this.data = data;
+
+        this.init = function () {
+            const parents = this.el.querySelectorAll('[data-parent]')
+
+            parents.forEach(parent => {
+                const open = parent.querySelector('[data-open]')
+
+                open.addEventListener('click', () => this.toggleItems(parent))
+            })
+        }
+
+        this.render = function () {
+            this.el.insertAdjacentHTML('beforeend', this.renderParent(this.data))
+        }
+
+        this.renderParent = function (data) {
+            if (data.hasChildren) {
+                return `<div class="list-item list-item_open" data-parent>
+                            <div class="list-item__inner">
+                                <img class="list-item__arrow" src="assets/img/chevron-down.png" alt="chevron-down" data-open>
+                                <img class="list-item__folder" src="assets/img/folder.png" alt="folder">
+                                <span>${data.name}</span>
+                            </div>
+                            <div class="list-item__items">
+                                ${data.items.name ? this.renderParent(data.items) : data.items.map(item => this.renderParent(item)).join('')}
+                            </div>
+                    </div>`
+            } else {
+                return this.renderChildren(data)
+            }
+        }
+
+        this.renderChildren = function (data) {
+            return `<div class="list-item">
+                        <div class="list-item__inner">
+                            <img class="list-item__folder" src="assets/img/folder.png" alt="folder">
+                            <span>${data.name}</span>
+                        </div>
+                    </div>`
+        }
+
+        this.toggleItems = function (parent) {
+            parent.classList.toggle('list-item_open')
+        }
+    }
+}
